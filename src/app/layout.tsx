7b40426c84679883659b8660f27a959764c4e68f
TheRAWagent <77039476+TheRAWagent@users.noreply.github.com>
2023-12-30 23:20:04 +0530

    implemented voice,video and text communication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,8 @@ import { ThemeProvider } from '@/providers/theme-provider'
 import { cn } from '@/lib/utils'
 import { ModalProvider } from "@/providers/ModalProvider";
 import { ourFileRouter } from "@/app/api/uploadthing/core";
+import { SocketProvider } from '@/providers/SocketProvider';
+import { QueryProvider } from '@/providers/QueryProvider';
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -28,8 +30,13 @@ export default function RootLayout({
         <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
           <ThemeProvider attribute='class' defaultTheme='system' enableSystem storageKey='discord-clone'>
             <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)}/>
+            <SocketProvider>
+            <QueryProvider>
+
             <ModalProvider />
             {children}
+            </QueryProvider>
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
--- src/components/server/ServerChannel.tsx
+++ src/components/server/ServerChannel.tsx
@@ -1,24 +1,24 @@
 "use client";
 
-import { ChanelType, Channel, MemberRole, server } from "@prisma/client";
+import { ChannelType, Channel, MemberRole, Server } from "@prisma/client";
 import { useParams, useRouter } from "next/navigation";
 
 import { Edit, Hash, Mic, Trash, Video,Lock } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { ActionTooltip } from "@/components/action-tooltip";
-import { useModal } from "@/hooks/useModalStore";
+import { ModalType, useModal } from "@/hooks/useModalStore";
 
 interface ServerChannelProps {
     Channel: Channel;
-    server: server;
+    server: Server;
     role?: MemberRole;
 }
 
 const IconMap = {
-    [ChanelType.TEXT]: Hash,
-    [ChanelType.AUDIO]: Mic,
-    [ChanelType.VIDEO]: Video
+    [ChannelType.TEXT]: Hash,
+    [ChannelType.AUDIO]: Mic,
+    [ChannelType.VIDEO]: Video
 }
 
 const ServerChannel = ({ Channel, server, role }: ServerChannelProps) => {
@@ -27,8 +27,16 @@ const ServerChannel = ({ Channel, server, role }: ServerChannelProps) => {
     const {onOpen} = useModal();
 
     const Icon = IconMap[Channel.type];
+
+    const onClick=()=>{
+        router.push(`/servers/${params?.serverId}/channels/${Channel.id}`);
+    }
+    const onAction=(e: React.MouseEvent,action: ModalType)=>{
+        e.stopPropagation();
+        onOpen(action,{server,channel: Channel});
+    }
     return (
-        <button className={cn("group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1", params?.channelId===Channel.id && "bg-zinc-700/20 dark:bg-zinc-700")}>
+        <button onClick={onClick} className={cn("group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1", params?.channelId===Channel.id && "bg-zinc-700/20 dark:bg-zinc-700")}>
             <Icon className=" flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400" />
             <p className={cn("line-clamp-1 font-semibold text-xs text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",params?.channelId===Channel.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white")}>
                 {Channel.name}
@@ -36,10 +44,10 @@ const ServerChannel = ({ Channel, server, role }: ServerChannelProps) => {
             {Channel.name!=="general" && role!==MemberRole.GUEST && (
                 <div className="ml-auto flex items-center gap-x-2">
                     <ActionTooltip label="Edit" align="center" side="right">
-                        <Edit onClick={()=>onOpen("editChannel",{server,channel: Channel})} className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"/>
+                        <Edit onClick={(e)=>onAction(e,"editChannel")} className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"/>
                     </ActionTooltip>
                     <ActionTooltip label="Delete" align="center" side="right">
-                        <Trash onClick={()=>onOpen("deleteChannel",{server,channel: Channel })} className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"/>
+                        <Trash onClick={(e)=>onAction(e,"deleteChannel")} className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"/>
                     </ActionTooltip>
 
                 </div>
